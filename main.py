6a8d8b18817df5423715a0110a0ae61195b59c9e
iulechklimkin@yandex.ru <iulechklimkin@yandex.ru>
2025-06-06 22:21:24 +0500

    поменяли порт и флаг и корабль

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ async def serve_static(file_path: str):
     return {"error": "File not found"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)  # <- Исправлено!
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)  # <- Исправлено!
